1d1d92c958fe2aefcb03c8af006186bfcc001756
gunheesong <g4song@uwaterloo.ca>
2025-08-08 20:29:31 -0400

    *Sveltekit practice 1* -Basic routing -Implementing +layouts, dynamic routing using [] Loading using the page.server.ts or the layout.server.ts, where its for fetching data and passing it to the  .svelte file before it renders.
    Basic blog functionality where it shows dynamically.

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { data } = $props();
+</script>
+
+<h1>blog post</h1>
+
+<ul>
+    {#each data.summaries as { slug, title}}
+
+        <li><a href="/blog/{slug}">{title}</a></li>
+
+    {/each}
+</ul>
---
+++ src/routes/blog/[slug]/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    let { data, children } = $props();
+
+</script>
+
+    <div>
+        <main>
+            {@render children()}    
+        </main>
+
+        <aside>
+            <h2>more posts</h2>
+            <ul>
+                {#each data.summaries as {slug, title}}
+                    <li><a href="/blog/{slug}">{title}</a></li>
+                {/each}
+            </ul>
+        </aside>
+    </div>
+    
